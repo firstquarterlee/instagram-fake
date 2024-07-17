a060d4cac6422ca858ba19bbd2f94b0893a79208
이상현 <firstquarterlee@gmail.com>
2024-07-18 01:20:34 +0900

    무한 스크롤 구현 중간 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+//import 'package:flutter/rendering.dart'; //스크롤 관련 유용한 함수들 들어있음
 
 void main() {
   runApp(MaterialApp(
@@ -33,11 +34,14 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var tab = 0; //현재 탭의 모습 //홈0, 샵1
+  var data = [];
 
   getData() async {
     var result =  await http.get(Uri.parse('https://codingapple1.github.io/app/data.json')); //get요청함
     var result2 = jsonDecode(result.body);
-    print(result2);
+    setState(() {
+      data = result2;
+    });
   }
 
   @override
@@ -59,7 +63,7 @@ class _MyAppState extends State<MyApp> {
           ],
         ),
       ),
-      body: [Home(), Text('홈페이지'), Text('샵페이지')][tab],
+      body: [Home(data: data), Text('홈페이지'), Text('샵페이지')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -80,31 +84,61 @@ class _MyAppState extends State<MyApp> {
 }
 
 //커스텀위젯
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key, this.data});
+
+  final data;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var scroll = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener((){
+      //print(scroll.position.pixels); //현재 스크롤 위치 알려줌
+      //print(scroll.position.maxScrollExtent); //최대 스크롤 가능한 길이 알려줌
+      //print(scroll.position.userScrollDirection); //위치 알려줌 위, 아래
+      if (scroll.position.pixels == scroll.position.maxScrollExtent) {
+        //맨 밑까지 왔을 때 알려줌 // 스크롤한 거리 == 최대 스크롤 거리
+        print('같음');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: 3, itemBuilder: (c, i){
-      return Column(
-        children: [
-          Image.network('https://codingapple1.github.io/kona.jpg'),
-          Container(
-            constraints: BoxConstraints(maxWidth: 600),
-            padding: EdgeInsets.all(20),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('좋아요 100'),
-                Text('글쓴이'),
-                Text('글내용'),
-              ],
-            ),
-          )
-        ],
-      );
-    });
+    if (widget.data.isNotEmpty) {
+      return ListView.builder(itemCount: 3, controller: scroll, itemBuilder: (c, i){
+        return Column(
+          children: [
+            Image.network('https://codingapple1.github.io/kona.jpg'),
+            Container(
+              constraints: BoxConstraints(maxWidth: 600),
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(widget.data[i]['image']),
+                  Text('좋아요 ${widget.data[i]['likes']}개'),
+                  Text(widget.data[i]['date']),
+                  Text(widget.data[i]['content']),
+                ],
+              ),
+            )
+          ],
+        );
+      });
+    } else {
+      return CircularProgressIndicator();
+
+    }
 
   }
 }
